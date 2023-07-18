5dbd5c2dd3814b49b1c61f4b622e46aa5dd4fb59
アカシイ - Akashii <44606942+AkashiiKun@users.noreply.github.com>
2023-07-18 11:27:35 +0100

    Support mc 1.20

--- src/main/java/top/leonx/irisflw/iris/GlUniformMcMatrix3f.java
+++ src/main/java/top/leonx/irisflw/iris/GlUniformMcMatrix3f.java
@@ -1,17 +1,14 @@
 package top.leonx.irisflw.iris;
 
-import com.mojang.math.Matrix3f;
-import com.mojang.math.Matrix4f;
+import org.joml.Matrix3f;
+import org.joml.Matrix4f;
 import me.jellysquid.mods.sodium.client.gl.shader.uniform.GlUniform;
 import org.lwjgl.opengl.GL30C;
 import org.lwjgl.system.MemoryStack;
 
 import java.nio.FloatBuffer;
 
-public class GlUniformMcMatrix3f extends GlUniform<Matrix3f>{
-
-    private static final FloatBuffer buf = MemoryStack.stackGet()
-            .mallocFloat(9);
+public class GlUniformMcMatrix3f extends GlUniform<Matrix3f> {
 
     public GlUniformMcMatrix3f(int index) {
         super(index);
@@ -19,7 +16,11 @@ public class GlUniformMcMatrix3f extends GlUniform<Matrix3f>{
 
 
     public void set(Matrix3f value) {
-        value.store(buf);
-        GL30C.glUniformMatrix3fv(this.index, false, buf);
+        try (MemoryStack stack = MemoryStack.stackPush()) {
+            FloatBuffer buf = stack.callocFloat(16);
+            value.get(buf);
+
+            GL30C.glUniformMatrix4fv(this.index, false, buf);
+        }
     }
 }
