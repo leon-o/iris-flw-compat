23f4200a96a6364eb2f5319aacf8ecfbe592c70b
Branan Riley <branan@top-secret.network>
2023-12-14 18:31:15 -0800

    Reserve the Create shadow volume sampler in Iris
    Create hardcodes sampler '4' for its contraption shader, to render
    light volumes.
    
    Fortunately, Iris has a pretty robust system in place to handle
    samplers used by other parts of the system.
    
    This hooks the Iris sampler setup to inject the sampler '4',
    ensuring that Iris will not attempt to use that sampler for its own
    purposes.
    
    Fixes #59
    
    (cherry picked from commit 66ccf27d795a15da3bed8d5e3649a16c3bfc453b)

---
+++ src/main/java/top/leonx/irisflw/mixin/MixinProgramSamplers.java
@@ -0,0 +1,22 @@
+package top.leonx.irisflw.mixin;
+
+import net.coderbot.iris.gl.program.ProgramSamplers;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.At;
+import java.util.Set;
+import java.util.HashSet;
+
+@Mixin(value = ProgramSamplers.class,remap = false)
+public class MixinProgramSamplers {
+    @ModifyVariable(method = "builder", at = @At("LOAD"), argsOnly = true)
+    private static Set<Integer> modifyReservedTextureUnits(Set<Integer> var1) {
+        // Add the lighting volume texture from Create to the reserved textures.
+        //
+        // The set from Iris is immutable, so duplicate before modifying.
+        Set<Integer> reservedTextureUnits = new HashSet<Integer>();
+        reservedTextureUnits.addAll(var1);
+        reservedTextureUnits.add(4);
+        return reservedTextureUnits;
+    }
+}
