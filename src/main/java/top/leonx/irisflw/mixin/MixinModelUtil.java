e19c58eb0ac27d2b41c66427f56956a876a8b298
Dunkmania101 <50186904+Dunkmania101@users.noreply.github.com>
2023-12-20 18:07:21 -0500

    Implement support for new method names in ModelUtil Mixin. (fixes crash with Create 0.5.1c+)

--- src/main/java/top/leonx/irisflw/mixin/MixinModelUtil.java
+++ src/main/java/top/leonx/irisflw/mixin/MixinModelUtil.java
@@ -20,23 +20,23 @@ import java.util.Collection;
 public class MixinModelUtil {
 
     @Group(name = "getBufferBuilderHead", min = 1, max = 2)
-    @Inject(method = "getBufferBuilder(Lcom/jozufozu/flywheel/core/model/Bufferable;)Lcom/jozufozu/flywheel/core/model/ShadeSeparatedBufferBuilder;", at = @At(value = "HEAD"),require = 0)
+    @Inject(method = {"getRenderedBuffer(Lcom/jozufozu/flywheel/core/model/Bufferable;)Lcom/jozufozu/flywheel/util/Pair;", "getBufferedData(Lcom/jozufozu/flywheel/core/model/Bufferable;)Lcom/jozufozu/flywheel/core/model/ShadeSeparatedBufferedData;"}, at = @At(value = "HEAD"),require = 0)
     private static void irisflw$getBufferBuilderHead(Bufferable bufferable, CallbackInfoReturnable<ShadeSeparatedBufferBuilder> cir) {
         BufferBuilderStateManager.setAllowExtend(false);
     }
 
     @Group(name = "getBufferBuilderTail", min = 1, max = 2)
-    @Inject(method = "getBufferBuilder(Lcom/jozufozu/flywheel/core/model/Bufferable;)Lcom/jozufozu/flywheel/core/model/ShadeSeparatedBufferBuilder;", at = @At(value = "TAIL"),require = 0)
+    @Inject(method = {"getRenderedBuffer(Lcom/jozufozu/flywheel/core/model/Bufferable;)Lcom/jozufozu/flywheel/util/Pair;", "getBufferedData(Lcom/jozufozu/flywheel/core/model/Bufferable;)Lcom/jozufozu/flywheel/core/model/ShadeSeparatedBufferedData;"}, at = @At(value = "TAIL"),require = 0)
     private static void irisflw$getBufferBuilderTail(Bufferable bufferable, CallbackInfoReturnable<ShadeSeparatedBufferBuilder> cir) {
         BufferBuilderStateManager.setAllowExtend(true);
     }
 
-    @Inject(method = "getBufferBuilderFromTemplate(Lnet/minecraft/world/level/BlockAndTintGetter;Lnet/minecraft/client/renderer/RenderType;Ljava/util/Collection;Lcom/mojang/blaze3d/vertex/PoseStack;)Lcom/jozufozu/flywheel/core/model/ShadeSeparatedBufferBuilder;", at = @At("HEAD"),require = 0)
+    @Inject(method = {"getBufferBuilderFromTemplate(Lnet/minecraft/world/level/BlockAndTintGetter;Lnet/minecraft/client/renderer/RenderType;Ljava/util/Collection;Lcom/mojang/blaze3d/vertex/PoseStack;)Lcom/jozufozu/flywheel/util/Pair;", "getBufferedDataFromTemplate(Lnet/minecraft/world/level/BlockAndTintGetter;Lnet/minecraft/client/renderer/RenderType;Ljava/util/Collection;Lcom/mojang/blaze3d/vertex/PoseStack;)Lcom/jozufozu/flywheel/core/model/ShadeSeparatedBufferedData;"}, at = @At("HEAD"),require = 0)
     private static void irisflw$getBufferBuilderFromTemplateHead(BlockAndTintGetter renderWorld, RenderType layer, Collection<StructureTemplate.StructureBlockInfo> blocks, PoseStack poseStack, CallbackInfoReturnable<ShadeSeparatedBufferBuilder> cir) {
         BufferBuilderStateManager.setAllowExtend(false);
     }
 
-    @Inject(method = "getBufferBuilderFromTemplate(Lnet/minecraft/world/level/BlockAndTintGetter;Lnet/minecraft/client/renderer/RenderType;Ljava/util/Collection;Lcom/mojang/blaze3d/vertex/PoseStack;)Lcom/jozufozu/flywheel/core/model/ShadeSeparatedBufferBuilder;", at = @At("RETURN"),require = 0)
+    @Inject(method = {"getBufferBuilderFromTemplate(Lnet/minecraft/world/level/BlockAndTintGetter;Lnet/minecraft/client/renderer/RenderType;Ljava/util/Collection;Lcom/mojang/blaze3d/vertex/PoseStack;)Lcom/jozufozu/flywheel/util/Pair;", "getBufferedDataFromTemplate(Lnet/minecraft/world/level/BlockAndTintGetter;Lnet/minecraft/client/renderer/RenderType;Ljava/util/Collection;Lcom/mojang/blaze3d/vertex/PoseStack;)Lcom/jozufozu/flywheel/core/model/ShadeSeparatedBufferedData;"}, at = @At("RETURN"),require = 0)
     private static void irisflw$getBufferBuilderFromTemplateTail(BlockAndTintGetter renderWorld, RenderType layer, Collection<StructureTemplate.StructureBlockInfo> blocks, PoseStack poseStack, CallbackInfoReturnable<ShadeSeparatedBufferBuilder> cir) {
         BufferBuilderStateManager.setAllowExtend(true);
     }
