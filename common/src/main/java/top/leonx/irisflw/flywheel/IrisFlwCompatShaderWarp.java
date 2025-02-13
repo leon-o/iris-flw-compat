4db10abc23100f3db38d8208de477900bea60b4f
Lonmo0208 <2675932757@qq.com>
2025-02-14 05:24:17 +0800

    = =

--- common/src/main/java/top/leonx/irisflw/flywheel/IrisFlwCompatShaderWarp.java
+++ common/src/main/java/top/leonx/irisflw/flywheel/IrisFlwCompatShaderWarp.java
@@ -8,10 +8,15 @@ import org.lwjgl.opengl.GL20;
 import top.leonx.irisflw.iris.GlUniformMcMatrix3f;
 import top.leonx.irisflw.iris.GlUniformMcMatrix4f;
 
+import java.lang.reflect.Field;
+import java.nio.ByteBuffer;
+import java.nio.FloatBuffer;
+
 public class IrisFlwCompatShaderWarp {
     public ShaderInstance shader;
     protected GlUniformMcMatrix4f uniformIrisProjMat;
     protected GlUniformMcMatrix4f iris_uniformModelViewMat;
+    //protected GlUniformMcMatrix4f uniformModelViewMat;
     protected GlUniformMcMatrix3f uniformNormalMatrix;
     protected GlUniformMcMatrix4f uniformModelViewProjMat;
 
@@ -19,79 +24,47 @@ public class IrisFlwCompatShaderWarp {
         this.shader = shader;
         int progId = shader.getId();
 
-        // Ensure MODEL_VIEW_MATRIX is properly initialized
-        if (shader.MODEL_VIEW_MATRIX == null) {
-            shader.MODEL_VIEW_MATRIX = new Uniform("ModelViewMat", 11, 17, shader);
-            shader.MODEL_VIEW_MATRIX.set(new Matrix4f().identity());
+        // Use reflection to modify the final field (not recommended)
+        try {
+            Field modelViewMatrixField = ShaderInstance.class.getDeclaredField("MODEL_VIEW_MATRIX");
+            modelViewMatrixField.setAccessible(true);
+            modelViewMatrixField.set(shader, new Uniform("ModelViewMat", 10, 16, shader));
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to modify MODEL_VIEW_MATRIX field", e);
         }
 
-        uniformIrisProjMat = new GlUniformMcMatrix4f(GL20.glGetUniformLocation(progId, "iris_ProjMat"));
-        iris_uniformModelViewMat = new GlUniformMcMatrix4f(GL20.glGetUniformLocation(progId, "iris_ModelViewMat"));
-        uniformNormalMatrix = new GlUniformMcMatrix3f(GL20.glGetUniformLocation(progId, "iris_NormalMat"));
-        uniformModelViewProjMat = new GlUniformMcMatrix4f(GL20.glGetUniformLocation(progId, "flw_ModelViewProjMat"));
+        uniformIrisProjMat = new GlUniformMcMatrix4f(GL20.glGetUniformLocation(progId,"iris_ProjMat"));
+        iris_uniformModelViewMat = new GlUniformMcMatrix4f(GL20.glGetUniformLocation(progId,"iris_ModelViewMat"));
+        uniformNormalMatrix = new GlUniformMcMatrix3f(GL20.glGetUniformLocation(progId,"iris_NormalMat"));
+        uniformModelViewProjMat = new GlUniformMcMatrix4f(GL20.glGetUniformLocation(progId,"flw_ModelViewProjMat"));
     }
 
     public void bind() {
         shader.apply();
-
-        // Enable depth testing and depth writing
-        GL20.glEnable(GL20.GL_DEPTH_TEST);
-        GL20.glDepthMask(true);
-
-        // Set projection and model-view matrices
         setProjectionMatrix(CapturedRenderingState.INSTANCE.getGbufferProjection());
         setModelViewMatrix(CapturedRenderingState.INSTANCE.getGbufferModelView());
-
-        // Ensure Shader uniform variables are set
-        updateShaderUniforms();
     }
 
-    public void unbind() {
+    public void unbind(){
         shader.clear();
-
-        // Restore depth testing and depth writing state
-        GL20.glDepthMask(false);
-        GL20.glDisable(GL20.GL_DEPTH_TEST);
     }
 
-    public int getProgramHandle() {
+    public int getProgramHandle(){
         return shader.getId();
     }
 
     public void setProjectionMatrix(Matrix4fc projectionMatrix) {
-        uniformIrisProjMat.set(new Matrix4f(projectionMatrix));
+        uniformIrisProjMat.set((Matrix4f) projectionMatrix);
     }
 
     public void setModelViewMatrix(Matrix4fc modelView) {
-        iris_uniformModelViewMat.set(new Matrix4f(modelView));
+        iris_uniformModelViewMat.set((Matrix4f) modelView);
 
         if (this.uniformNormalMatrix != null) {
             Matrix4f normalMatrix = new Matrix4f(modelView);
             normalMatrix.invert();
             normalMatrix.transpose();
             this.uniformNormalMatrix.set(new Matrix3f(normalMatrix));
-        }
-
-        // Ensure MODEL_VIEW_MATRIX is updated
-        shader.MODEL_VIEW_MATRIX.set(new Matrix4f(modelView));
-    }
-
-    private void updateShaderUniforms() {
-        // Ensure all Shader uniform variables are set
-        uniformIrisProjMat.set(new Matrix4f(CapturedRenderingState.INSTANCE.getGbufferProjection()));
-        iris_uniformModelViewMat.set(new Matrix4f(CapturedRenderingState.INSTANCE.getGbufferModelView()));
-
-        if (uniformNormalMatrix != null) {
-            Matrix4f normalMatrix = new Matrix4f(CapturedRenderingState.INSTANCE.getGbufferModelView());
-            normalMatrix.invert();
-            normalMatrix.transpose();
-            uniformNormalMatrix.set(new Matrix3f(normalMatrix));
-        }
-
-        if (uniformModelViewProjMat != null) {
-            Matrix4f modelViewProjMatrix = new Matrix4f(CapturedRenderingState.INSTANCE.getGbufferProjection())
-                    .mul(CapturedRenderingState.INSTANCE.getGbufferModelView());
-            uniformModelViewProjMat.set(modelViewProjMatrix);
         }
     }
 }
--- common/src/main/java/top/leonx/irisflw/iris/GlUniformMcMatrix4f.java
+++ common/src/main/java/top/leonx/irisflw/iris/GlUniformMcMatrix4f.java
@@ -2,28 +2,18 @@ package top.leonx.irisflw.iris;
 
 import me.jellysquid.mods.sodium.client.gl.shader.uniform.GlUniform;
 import org.joml.Matrix4f;
-import org.joml.Matrix4fc;
 import org.lwjgl.opengl.GL30C;
 import org.lwjgl.system.MemoryStack;
 
 import java.nio.FloatBuffer;
 
-public class GlUniformMcMatrix4f extends GlUniform<Matrix4f>{
+public class GlUniformMcMatrix4f extends GlUniform<Matrix4f> {
     public GlUniformMcMatrix4f(int index) {
         super(index);
     }
 
-
+    @Override
     public void set(Matrix4f value) {
-        try (MemoryStack stack = MemoryStack.stackPush()) {
-            FloatBuffer buf = stack.callocFloat(16);
-            value.get(buf);
-
-            GL30C.glUniformMatrix4fv(this.index, false, buf);
-        }
-    }
-
-    public void set(Matrix4fc value) {
         try (MemoryStack stack = MemoryStack.stackPush()) {
             FloatBuffer buf = stack.callocFloat(16);
             value.get(buf);
