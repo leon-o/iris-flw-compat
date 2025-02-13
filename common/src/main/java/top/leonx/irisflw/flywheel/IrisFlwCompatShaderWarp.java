48b40145378d9adbc139918579cf031221b87b05
Lonmo <95102577+Lonmo0208@users.noreply.github.com>
2025-02-14 02:46:41 +0800

    Update IrisFlwCompatShaderWarp.java Fix

--- common/src/main/java/top/leonx/irisflw/flywheel/IrisFlwCompatShaderWarp.java
+++ common/src/main/java/top/leonx/irisflw/flywheel/IrisFlwCompatShaderWarp.java
@@ -1,20 +1,20 @@
 package top.leonx.irisflw.flywheel;
 
 import com.mojang.blaze3d.shaders.Uniform;
-import org.joml.Matrix3f;
-import org.joml.Matrix4f;
-import org.joml.Matrix4fc;
+import org.joml.*;
 import net.irisshaders.iris.uniforms.CapturedRenderingState;
 import net.minecraft.client.renderer.ShaderInstance;
 import org.lwjgl.opengl.GL20;
 import top.leonx.irisflw.iris.GlUniformMcMatrix3f;
 import top.leonx.irisflw.iris.GlUniformMcMatrix4f;
 
+import java.nio.ByteBuffer;
+import java.nio.FloatBuffer;
+
 public class IrisFlwCompatShaderWarp {
     public ShaderInstance shader;
     protected GlUniformMcMatrix4f uniformIrisProjMat;
     protected GlUniformMcMatrix4f iris_uniformModelViewMat;
-    //protected GlUniformMcMatrix4f uniformModelViewMat;
     protected GlUniformMcMatrix3f uniformNormalMatrix;
     protected GlUniformMcMatrix4f uniformModelViewProjMat;
 
@@ -22,17 +22,17 @@ public class IrisFlwCompatShaderWarp {
         this.shader = shader;
         int progId = shader.getId();
 
-        //ModelViewMat may be removed if the shader doesn't use it.
-        //If the MODEL_VIEW_MATRIX is null, the game will crash when we call ExtendedShader::apply().
-        if(shader.MODEL_VIEW_MATRIX == null){
-            shader.MODEL_VIEW_MATRIX = new Uniform("ModelViewMat",10,16,shader);
-            shader.MODEL_VIEW_MATRIX.set(new Matrix4f());
+        // Instead of modifying the final field, we create a new Uniform instance
+        // and ensure it is properly initialized.
+        if (shader.MODEL_VIEW_MATRIX == null) {
+            shader.MODEL_VIEW_MATRIX = new Uniform("ModelViewMat", 11, 17, shader);
+            shader.MODEL_VIEW_MATRIX.set(new Matrix4f().identity());
         }
 
-        uniformIrisProjMat = new GlUniformMcMatrix4f(GL20.glGetUniformLocation(progId,"iris_ProjMat"));
-        iris_uniformModelViewMat = new GlUniformMcMatrix4f(GL20.glGetUniformLocation(progId,"iris_ModelViewMat"));
-        uniformNormalMatrix = new GlUniformMcMatrix3f(GL20.glGetUniformLocation(progId,"iris_NormalMat"));
-        uniformModelViewProjMat = new GlUniformMcMatrix4f(GL20.glGetUniformLocation(progId,"flw_ModelViewProjMat"));
+        uniformIrisProjMat = new GlUniformMcMatrix4f(GL20.glGetUniformLocation(progId, "iris_ProjMat"));
+        iris_uniformModelViewMat = new GlUniformMcMatrix4f(GL20.glGetUniformLocation(progId, "iris_ModelViewMat"));
+        uniformNormalMatrix = new GlUniformMcMatrix3f(GL20.glGetUniformLocation(progId, "iris_NormalMat"));
+        uniformModelViewProjMat = new GlUniformMcMatrix4f(GL20.glGetUniformLocation(progId, "flw_ModelViewProjMat"));
     }
 
     public void bind() {
@@ -41,14 +41,15 @@ public class IrisFlwCompatShaderWarp {
         setModelViewMatrix(CapturedRenderingState.INSTANCE.getGbufferModelView());
     }
 
-    public void unbind(){
+    public void unbind() {
         shader.clear();
     }
 
-    public int getProgramHandle(){
+    public int getProgramHandle() {
         return shader.getId();
     }
-    public void setProjectionMatrix(Matrix4fc projectionMatrix){
+
+    public void setProjectionMatrix(Matrix4fc projectionMatrix) {
         uniformIrisProjMat.set(projectionMatrix);
     }
 
