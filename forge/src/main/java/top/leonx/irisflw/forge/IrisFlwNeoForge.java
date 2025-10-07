6894e01a33ef38e4a00717899adaa42c183b00ef
leon-o <leonecho@outlook.com>
2025-10-07 15:58:09 +0800

    Merge branch 'main' into 1.20.1-2.0

--- forge/src/main/java/top/leonx/irisflw/forge/IrisFlwNeoForge.java
+++ forge/src/main/java/top/leonx/irisflw/forge/IrisFlwNeoForge.java
@@ -1,12 +1,37 @@
 package top.leonx.irisflw.forge;
 
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import top.leonx.irisflw.IrisFlw;
+import static net.minecraftforge.common.MinecraftForge.EVENT_BUS;
 
 @Mod(IrisFlw.MOD_ID)
 public final class IrisFlwNeoForge {
     public IrisFlwNeoForge() {
         // Run our common setup.
         IrisFlw.init();
+        CompatibilityCheck.register();
+    }
+
+    @EventBusSubscriber
+    public static class CompatibilityCheck {
+
+        @SubscribeEvent
+        public static void onPlayerLoggedIn(PlayerEvent.PlayerLoggedInEvent event) {
+            if (ModList.get().isLoaded("scalablelux")) {
+                if (event.getEntity() instanceof net.minecraft.server.level.ServerPlayer player) {
+                    player.sendSystemMessage(Component.literal(ChatFormatting.DARK_RED+"IrisFlwCompat is not compatible with ScalableLux!"));
+                }
+            }
+        }
+
+        public static void register() {
+            EVENT_BUS.register(CompatibilityCheck.class);
+        }
     }
 }
